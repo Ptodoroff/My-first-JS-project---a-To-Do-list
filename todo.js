2ca03765563ea291e47e14a0649778e3fa1301c2
Petar Todoroff <p.todorov212@gmail.com>
2022-07-08 11:45:47 +0300

    will store the tasks in an array from now on

--- todo.js
+++ todo.js
@@ -2,6 +2,9 @@ let date=new Date();
 let currentDate=document.getElementById('currentDate');
 currentDate.innerHTML=`<u>${date.toLocaleString()}</u>`;
 
+
+let taskArray=[];
+let tasks=[];
 let $taskList=document.querySelector('.taskList');
 let $checkMark=document.querySelector('#checkbox');
 let $task=document.querySelector('#taskBox');
@@ -9,7 +12,8 @@ let $btn=document.querySelector('[name="btn-1"]');
 $btn.addEventListener('click',saveTask);
 function saveTask (event){
     event.preventDefault();
-     $taskList.innerHTML+= `<tr><td>${$task.value}</td><td><input type="checkbox"></td> <td> <button> Delete </button> </td></tr>`
-     
+     $taskList.innerHTML+= `<tr><td>${$task.value}</td><td><input type="checkbox"></td> <td> <button id='btn-2'> Delete </button> </td></tr>`
+     taskArray.push($task.value)
+     console.log(taskArray);
 
 }
