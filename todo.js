4d01a34e46b3fcdc3d514b4255055fad28fa4267
Petar Todoroff <p.todorov212@gmail.com>
2022-07-08 18:08:49 +0300

    successfully added a strikethrough feature for a completed task upon clicking the Complete button. The last remaining task is to add local storage to the list, so that the task are saved after every session. Also, some basic design improvements could be added

--- todo.js
+++ todo.js
@@ -2,18 +2,50 @@ let date=new Date();
 let currentDate=document.getElementById('currentDate');
 currentDate.innerHTML=`<u>${date.toLocaleString()}</u>`;
 
+let todo={
+    id: '',
+    name: '',
+    completed: false
+}
 
-let taskArray=[];
+function addTodo(item) {
+    if (item!=='') {
+        const todo={
+            id: String (Date()),
+            name: item,
+            completed: false
+        }
+        tasks.push(todo);
+        console.log(tasks);
+        $taskList.innerHTML=tasks.map(todo=>`<li class="${todo.id} listItem">
+                              ${todo.name}
+                              <button type='submit' class="complete" > Complete </button>
+                              </li>`).join('');
+    }
+   
+
+
+for (let $complete of $taskList.querySelectorAll('.complete')) {
+    $complete.addEventListener('click' ,checkCompleted);
+    function checkCompleted (event) {
+        
+        console.log(todo.completed);
+        console.log(event.target);
+         $complete.parentNode.classList.toggle("checked");
+    }
+}
+
+
+
+}
+
 let tasks=[];
 let $taskList=document.querySelector('.taskList');
-let $checkMark=document.querySelector('#checkbox');
-let $task=document.querySelector('#taskBox');
+let $task=document.querySelector('#todo-input');
+let $todoItemsLsit=document.querySelector('.item')
 let $btn=document.querySelector('[name="btn-1"]');
 $btn.addEventListener('click',saveTask);
 function saveTask (event){
     event.preventDefault();
-     $taskList.innerHTML+= `<tr><td>${$task.value}</td><td><input type="checkbox"></td> <td> <button id='btn-2'> Delete </button> </td></tr>`
-     taskArray.push($task.value)
-     console.log(taskArray);
-
+    addTodo($task.value)
 }
