dace3dc618c59f75a340b0004e4c22aa9538b36a
Petar Todoroff <p.todorov212@gmail.com>
2022-07-10 14:46:02 +0300

    fixed the buttons and they now work

--- todo.js
+++ todo.js
@@ -2,55 +2,85 @@ let date=new Date();
 let currentDate=document.getElementById('currentDate');
 currentDate.innerHTML=`<u>${date.toLocaleString()}</u>`;
 
+
+let tasks;
+tasks=tasks? JSON.parse(localStorage.getItem("tasks")):[];
+let $taskList=document.querySelector('.taskList');
+let $task=document.querySelector('#todo-input');
+let $todoItemsLsit=document.querySelector('.item')
+let $btn=document.querySelector('[name="btn-1"]');
+
 let todo={
     id: '',
     name: '',
     completed: false
-}
+};
+        function getLS(){
+            if (tasks) {
+                $taskList.innerHTML=tasks.map(todo=>`<li class="listItem" data-key="${todo.id}">
+                ${todo.name}
+                <button type='submit' class="complete" > Complete </button> <button class='delete'> Delete task </button>
+                </li>`).join('');
+        }
+    }   
+
+
+
+getLS();
+console.log(tasks);
 
 function addTodo(item) {
     if (item!=='') {
-        const todo={
-            id: String (Date()),
+         todo={
+            id: JSON.stringify(Date.now()),
             name: item,
             completed: false
         }
-        tasks.push(todo);
-        console.log(tasks);
-        $taskList.innerHTML=tasks.map(todo=>`<li class="${todo.id} listItem">
+         
+        function addTask () {
+            localStorage.setItem('todo', JSON.stringify(todo))
+            tasks.push(todo);
+            localStorage.setItem('tasks', JSON.stringify(tasks))
+        }
+        addTask();
+        
+       
+
+
+        $taskList.innerHTML+=tasks.map(todo=>`<li class="listItem" data-key="${todo.id}">
                               ${todo.name}
                               <button type='submit' class="complete" > Complete </button> <button class='delete'> Delete task </button>
                               </li>`).join('');
     }
    
-
-
-for (let $complete of $taskList.querySelectorAll('.complete')) {
-    $complete.addEventListener('click' ,checkCompleted);
-    function checkCompleted (event) {
-        
-        console.log(todo.completed);
-        console.log(event.target);
-         $complete.parentNode.classList.toggle("checked");
-         $complete.classList.toggle("checked");
+    for (let $complete of $taskList.querySelectorAll('.complete')) {
+        $complete.addEventListener('click' ,checkCompleted);
+        function checkCompleted (event) {
+            
+            console.log(todo.completed);
+            console.log(event.target);
+             $complete.parentElement.classList.toggle("checked");
+             $complete.classList.toggle("checked");
+        }
     }
-}
-
-for (let $delete of $taskList.querySelectorAll('.delete')){
-    $delete.addEventListener('click',deleteCompleted)
-    function deleteCompleted (){
-        $delete.parentElement.remove();
+    
+    for (let $delete of $taskList.querySelectorAll('.delete')){
+        $delete.addEventListener('click',deleteCompleted)
+        function deleteCompleted (){
+            $delete.parentElement.remove();
+        }
     }
+    
+
+
+
+
+
 }
 
 
-}
 
-let tasks=[];
-let $taskList=document.querySelector('.taskList');
-let $task=document.querySelector('#todo-input');
-let $todoItemsLsit=document.querySelector('.item')
-let $btn=document.querySelector('[name="btn-1"]');
+
 $btn.addEventListener('click',saveTask);
 function saveTask (event){
     event.preventDefault();
